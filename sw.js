f633306c3fc0502f5fd88a077f3c6a6bb7f24833
Adejbakhsh <arman@dejbakhsh.fr>
2022-03-14 15:55:20 +0100

    install

--- sw.js
+++ sw.js
@@ -1,3 +1,19 @@
 self.addEventListener('fetch', function() {
     return;
 });
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
