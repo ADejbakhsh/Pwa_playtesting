bcaf43a085e4394816735344ccdcd21a15e4bf46
Adejbakhsh <arman@dejbakhsh.fr>
2022-03-14 17:03:11 +0100

    offline ?

--- sw.js
+++ sw.js
@@ -1,19 +1,30 @@
-self.addEventListener('fetch', function() {
-    return;
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
 });
 
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/',
+const CACHE_NAME = 'hello-pwa-v1';
+const urlsToCache = [
+    '/index.html',
+    '/'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
